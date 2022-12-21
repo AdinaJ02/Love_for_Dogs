731b4f5d6a13b246f02e347b38fcc2c1a15f60d4
AdinaJ02 <adina.jacob15@gmail.com>
2022-12-21 20:33:43 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Love For Dogs',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
